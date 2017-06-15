43f347c24daf9c805e89c20ff5c08fdf7e23f6d6
brayand <bray.andrew.0@gmail.com>
2017-06-15 18:46:17 -0500

    Add selection options

--- lookupEngine.js
+++ lookupEngine.js
@@ -2,6 +2,29 @@
 var submitClicked = document.getElementById("submitButton")
 
 submitClicked.addEventListener("click", infoReturn);
+    
+
+var inputState = document.getElementById("State");
+var states = [];
+data.forEach(function(dist,i){
+	var option = document.createElement("option");
+	if(states.indexOf(dist.state)<0){
+		states.push(dist.state);
+		option.text = dist.state;
+		inputState.add(option);
+	}
+})
+
+var inputDist = document.getElementById("District");
+var dists = [];
+data.forEach(function(dist,i){
+	var option = document.createElement("option");
+	if(dists.indexOf(dist.district)<0){
+		dists.push(dist.district);
+		option.text = dist.district;
+		inputDist.add(option);
+	}
+})
 
 
 function infoReturn(){
